62d23a9ae330d395f1c0037c622b2a173f4715e8
Gary Lewman <glewman18@yahoo.com>
2021-03-15 12:47:34 -0500

    login/navbar

--- components/views/Login.js
+++ components/views/Login.js
@@ -1,3 +1,37 @@
 import html from "html-literal";
 
-export default () => html` <p>Login Page</p>`;
+export default () => html`
+  <div class="container">
+    <form id="logIn" method="POST" action="">
+      <p id="loginText">Login</p>
+      <div>
+        <label for="username">User name:</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Username"
+        />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+        />
+      </div>
+      <div>
+        <label for="email">Email:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email Address"
+        />
+      </div>
+      <input type="submit" name="register" value="Register" />
+    </form>
+  </div>
+`;
