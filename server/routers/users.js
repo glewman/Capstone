894803c93da1658289291ac1d32532c5612cf565
Gary Lewman <glewman18@yahoo.com>
2021-03-18 14:36:35 -0500

    MongoDB

---
+++ server/routers/users.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const router = Router();
+
+router
+  // routes will be specified at the app-level
+  .route("/") // equivalent to /users
+  .get((request, response) => {
+    response.json({ user: "Gary" });
+  })
+  .post((request, response) => {
+    response.json(request.body);
+  });
+//Router Object
+module.exports = router;
