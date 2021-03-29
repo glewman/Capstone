bec65c2282bbd258ab90659daabd479090236679
Gary Lewman <glewman18@yahoo.com>
2021-03-28 23:40:38 -0500

    uploaded pics

--- store/Pictures.js
+++ store/Pictures.js
@@ -4,13 +4,33 @@ export default {
   pictures: [
     {
       url:
-        "https://trello-attachments.s3.amazonaws.com/601d8a245279db558f88542e/602cb70f7f46a92fe75b80e9/0b158cd1acd88094987e3ce2314bc5f4/capstone_angelfish_1.jpg",
-      title: "angelfish",
+        "https://trello-attachments.s3.amazonaws.com/601d8a245279db558f88542e/602cb70f7f46a92fe75b80e9/e62309160740d57c73a40abd5b5b2790/0205212116.jpg",
+      title: "1st saltwater",
     },
     {
       url:
-        "https://trello-attachments.s3.amazonaws.com/601d8a245279db558f88542e/602cb70f7f46a92fe75b80e9/d630a4f2b900a6fd0d9c83f37ff0f20e/capstone_saltwater_tank_2.jpg",
-      title: "reeftank",
+        "https://trello-attachments.s3.amazonaws.com/601d8a245279db558f88542e/602cb70f7f46a92fe75b80e9/07949040ba4f8226ac16d1d2b7425240/0514201607_HDR.jpg",
+      title: "1st freshwater",
+    },
+    {
+      url:
+        "https://trello-attachments.s3.amazonaws.com/601d8a245279db558f88542e/602cb70f7f46a92fe75b80e9/9a0e18bd279cfe4683a534269dbaa317/0107201223.jpg",
+      title: "saltwaterJan2020",
+    },
+    {
+      url:
+        "https://trello-attachments.s3.amazonaws.com/601d8a245279db558f88542e/602cb70f7f46a92fe75b80e9/5670ad6b0e712bbedfeb38ef52d9ba85/0113201625a_HDR.jpg",
+      title: "freshwater build",
+    },
+    {
+      url:
+        "https://trello-attachments.s3.amazonaws.com/601d8a245279db558f88542e/602cb70f7f46a92fe75b80e9/7cd4763e97cc839f8815895c7515232c/0113201625_HDR.jpg",
+      title: "freshwater build 2",
+    },
+    {
+      url:
+        "https://trello-attachments.s3.amazonaws.com/601d8a245279db558f88542e/602cb70f7f46a92fe75b80e9/76d5f18d28238a4b79664bb6f7a64050/0328212337.jpg",
+      title: "3-26 saltwater",
     },
   ],
 };
