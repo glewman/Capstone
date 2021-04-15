af0c9ec7830e300840c254370e0cb043d0c24879
Gary Lewman <glewman18@yahoo.com>
2021-04-15 14:08:02 -0500

    updated about page

--- components/views/About.js
+++ components/views/About.js
@@ -4,7 +4,7 @@ export default () => html`
 <div id="aboutHeader"><p>About/Contact Us</p></div>
 <p>
     <h3>
-        I created this app to be an organizational tool that helps you track any and all changes you would make to your home aquarium and document the outcomes. One thing I've learned over the years is that there are so many different things that go into a healthy aquarium, and all of them require their own maintenance at different times. I wanted to come up with a more mobile, convenient way to keep track of all of these things beyond an unorganized notebook. Thus, an idea was born.
+        I created this app to be an organizational tool that helps you track any and all changes you would make to your home aquarium and document the outcomes. One thing I've learned over the years is that there are so many different things that go into a healthy aquarium, and all of them require their own maintenance at different times. To get the most from this application make sure to log as much useful information s possible. Things like the date, location of purchase and price will all be useful information. Taking pictures on a regular basis will also help you notice reoccurring problem areas and growth rates. This way you can track if the money you have spent has been a worthwhile investment or a beneficial change to you system.
     </h3>
 
     <h3>Contact us at: Tankmate123@yahoo.com</h3>
